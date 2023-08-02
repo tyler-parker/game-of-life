d7a83ae49b9772b6ba0b933a734145dba678b3ce
tyler parker <tparker42d2@gmail.com>
2023-08-02 12:35:13 -0600

    Fixed CSS, made sizing responsive

--- src/app/gol-header/gol-header.component.ts
+++ src/app/gol-header/gol-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { GameOfLife } from './game';
 
 @Component({
@@ -10,9 +10,24 @@ export class GOLHeaderComponent implements OnInit {
   game!: GameOfLife;
   interval: any;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.adjustGridSize();
+  }
+
   ngOnInit() {
     this.game = new GameOfLife(50, 50); // Grid size 50x50
     this.interval = setInterval(() => this.game.nextGeneration(), 100);
+  }
+
+  adjustGridSize() {
+    const cellWidth = 10; // width of a cell in pixels
+    const cellHeight = 10; // height of a cell in pixels
+    const headerHeight = window.innerHeight * 0.2; // 20% of the viewport height
+    const numberOfColumns = Math.floor(window.innerWidth / cellWidth);
+    const numberOfRows = Math.floor(headerHeight / cellHeight);
+
+    this.game = new GameOfLife(numberOfRows, numberOfColumns);
   }
 
   ngOnDestroy() {
